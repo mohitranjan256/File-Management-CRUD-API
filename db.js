da5e1ad121f6e01f6210e3ee9c93fce91f3eeffc
Mohit Ranjan <84243917+monster256@users.noreply.github.com>
2022-03-17 19:14:36 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose');
 const mongodb = require('mongodb')
-const mongoURI="mongodb://localhost:27017/file-management?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
+const atlasURL =process.env.ATLAS_URL;
 const connecttOMongo=async()=>{
-    mongoose.connect(mongoURI,()=>{
+    mongoose.connect(atlasURL,()=>{
         console.log("Connected to MongoDB");
     })
 }
