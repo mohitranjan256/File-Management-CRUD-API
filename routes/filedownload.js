38ec935a4aa4d1047f464477151957315cbc389d
Mohit Ranjan <84243917+monster256@users.noreply.github.com>
2022-03-17 20:35:52 +0530

    Update filedownload.js

--- routes/filedownload.js
+++ routes/filedownload.js
@@ -3,12 +3,12 @@ const routes = express.Router()
 const files = require('../model/fileschemma');
 const path = require('path')
 
-
 routes.get('/:id', async(req,res)=>{
-    const downloadid = req.params.id
+    
     try{
+        const downloadid = req.params.id
         const downloadfile = await files.findById(downloadid)
-        const filepath = path.join(__dirname,"../Images",downloadfile.Filename)
+        const filepath = path.join(__dirname,"../Images",downloadfile.File_Name)
         res.download(filepath)
         // console.log(downloadfile)
 
