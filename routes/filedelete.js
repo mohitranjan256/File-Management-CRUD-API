ecd11575e30e5a7c92f99387d4bb64ed14df3f13
Mohit Ranjan <84243917+monster256@users.noreply.github.com>
2022-03-17 20:31:27 +0530

    Update filedelete.js

--- routes/filedelete.js
+++ routes/filedelete.js
@@ -3,26 +3,30 @@ const routes = express.Router()
 const files = require('../model/fileschemma');
 const path = require('path');
 const { unlinkSync } = require('fs');
+const { findByIdAndDelete } = require('../model/fileschemma');
+const fs = require('fs')
 
 
 routes.get('/:id', async(req,res)=>{
-    const downloadid = req.params.id
     try{
+        const downloadid = req.params.id
         const downloadfile = await files.findById(downloadid)
-        const filepath = path.join(__dirname,"../Images",downloadfile.Filename)
-        if(!filepath){
-            res.send("There is no such file to Delete..")
+        const filepath = path.join(__dirname,"../Images",downloadfile.File_Name)
+        if(fs.existsSync(filepath)){
+            console.log("this point")
+            const removefile = await fs.unlinkSync(filepath)
+            const removeData = await files.findByIdAndDelete(downloadid)
+            return res.send("File Deleted Successfully..")
         }
-        else{
-            const removefile = await unlinkSync(filepath)
-            res.send("File Deleted Succesfully..")
-
-        }
+        
 
     }
-    catch(err){
-        res.send("File Cannot be Deleted !! ")
-    }
-})
+        catch(err){
+            console.log(err)
+           return res.send("File Cannot be Updated !! ")
+            
+        }
+    })
+
 
 module.exports = routes
